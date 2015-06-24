14c9387794e7ab0e001da9f09b9c199f86ffb976
mduqueoviedo@gmail.com <mduqueoviedo@gmail.com>
2015-06-24 12:35:32 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  PLACES = [
+            "Belushi's",
+            "Meat Place Under the Bridge",
+            "Asian Place formerly know as Udon Mafia Place",
+            "THE Vietnamese",
+            "Austrian Nikolai Place",
+            "Pizza a Posto",
+            "Dolores",
+            "Esra"
+          ];
+
+  //Show all places to pick which ones will be selected
+  var items = [];
+  $.each(PLACES, function(i, item) {
+    items.push('<li><input class="place-checkbox" type="checkbox" value="' + i + '">&nbsp;' + item + '</li>');
+  });
+  $('#places_list').append( items.join('') );
+  $("#result_window").val(Cookies.get("luncher_previous"));
+
+  $("#luncher_button").click(function() {
+    selected_places = $("input.place-checkbox:checked");
+    result = shuffle(selected_places);
+    $("#result_window").val();
+    $("#result_window").val(PLACES[result.value]);
+    Cookies.set("luncher_previous", result.value);
+  });
+
+});
+
+// Shuffle array content
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array[0];
+}
